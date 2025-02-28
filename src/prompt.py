0a591af0d01890642d81b510d41c96b65d5c2b20
Gaurav Pandey <167669943+Gaurav-1111@users.noreply.github.com>
2025-02-28 09:48:57 +0000

    initial commit

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+
+system_prompt = (
+    "You are an assistant for question-answering tasks."
+    "Use the following pieces of retrieved context to answer"
+    "If you dont know the answer, say dont know"
+    "Use five sentences maximum and keep the answer concise"
+    "\n\n"
+    "{context}"
+)
